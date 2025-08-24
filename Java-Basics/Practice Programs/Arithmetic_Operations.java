b7b8ded4906a18a4d61881a7ab930764481c9b6c
VighneshwarH <vighneshwarh5487@gmail.com>
2025-08-24 13:02:06 +0530

    Added a Java File

---
+++ Java-Basics/Practice Programs/Arithmetic_Operations.java
@@ -0,0 +1,16 @@
+public class Arithmetic_Operations {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        int product = num1 * num2;
+        double quotient = (double) num1 / num2;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+    }
+}
