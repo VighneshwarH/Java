2dde09a8632149fafe81178e3f58e2ffe80053a1
VighneshwarH <vighneshwarh5487@gmail.com>
2025-08-24 12:59:19 +0530

    Added a Program

---
+++ Java-Basics/Practice Programs/Person_Details.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Person_Details{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
+        System.out.print("Enter your height (in meters): ");
+        double height = sc.nextDouble();
+
+        System.out.println("\n--- Person Details ---");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " meters");
+
+        sc.close();
+    }
+}
